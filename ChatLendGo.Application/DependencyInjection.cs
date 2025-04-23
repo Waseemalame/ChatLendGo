4c18b17334f16e0ec479aee5f7abbcc12aa9b2e0
Waseem Alame <walame@its4logistics.com>
2025-04-22 18:22:06 -0700

    Add DI setup

---
+++ ChatLendGo.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using ChatLendGo.Application.Interfaces;
+using ChatLendGo.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatLendGo.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IConversationService, ConversationService>();
+        services.AddScoped<IModelSelectionService, ModelSelectionService>();
+        services.AddScoped<IVoiceInteractionService, VoiceInteractionService>();
+
+        return services;
+    }
+}
